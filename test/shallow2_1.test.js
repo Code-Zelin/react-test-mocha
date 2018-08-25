57360d1260d9515b97aff3ef2e7641b96cec9a0b
szl <463515279@qq.com>
2018-08-25 14:33:38 +0800

    test

--- test/shallow2_1.test.js
+++ test/shallow2_1.test.js
@@ -12,7 +12,7 @@ function shallowRender(Component, props) {
 describe('检测组件是否可用', function() {
   it('该组件正常情况下不应该有todo-done类名', function() {
     const todoItemData = {
-      id: 0,
+      id: 1,
       name: 'abc',
       done: false,
     };
